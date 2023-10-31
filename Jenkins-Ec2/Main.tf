7e84fdaa0464cd022916cf29d9c22863345964cd
Velocity9919 <143435067+Velocity9919@users.noreply.github.com>
2023-10-31 21:12:30 +0530

    Update Main.tf

--- Jenkins-Ec2/Main.tf
+++ Jenkins-Ec2/Main.tf
@@ -1,46 +1,170 @@
-resource "aws_security_group" "Jenkins-sg" {
-  name        = "Jenkins-Security Group"
-  description = "Open 22,443,80,8080"
+provider "aws" {
+  region = var.location
+}
 
-  # Define a single ingress rule to allow traffic on all specified ports
-  ingress = [
-    for port in [22, 80, 443, 8080] : {
-      description      = "TLS from VPC"
-      from_port        = port
-      to_port          = port
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
+resource "aws_instance" "Jenkins-instance" {
+  ami           = var.os-name
+  instance_type = var.instance-type
+  key_name = var.key-pair
+  subnet_id = aws_subnet.Demo-web-public-subent-1a.id
+  vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
+  user_data              = templatefile("./install_Ansible.sh", {})
+
+ tags = {
+    Name = "Jenkins-instance"
+  }
+}
+
+# creating vpc
+
+resource "aws_vpc" "Demo-vpc" {
+    cidr_block = var.vpc-cidr
+    tags = {
+      Name = "Demo-vpc"
     }
-  ]
+  
+}
+
+resource "aws_subnet" "Demo-web-public-subent-1a" {
+    vpc_id = "${aws_vpc.Demo-vpc.id}"
+    cidr_block = var.subnet1-cidr
+    map_public_ip_on_launch = "true"
+    availability_zone = var.subent_az1
+    tags = {
+      Name = "Demo-web-public-subent-1a"
+    }
+  
+}
+
+resource "aws_subnet" "Demo-app-public-subent-1a" {
+    vpc_id = "${aws_vpc.Demo-vpc.id}"
+    cidr_block = var.subnet2-cidr
+    map_public_ip_on_launch = "true"
+    availability_zone = var.subent_az2
+    tags = {
+      Name = "Demo-app-public_subent_1a"
+    }
+  
+}
+
+resource "aws_subnet" "Demo-db-private-subent-1a" {
+    vpc_id = "${aws_vpc.Demo-vpc.id}"
+    cidr_block = var.subnet3-cidr
+    map_public_ip_on_launch = "true"
+    availability_zone = var.subent_az3
+    tags = {
+      Name = "Demo-db-public_subent_1a"
+    }
+  
+}
+
+resource "aws_subnet" "Demo-web-public-subent-1b" {
+    vpc_id = "${aws_vpc.Demo-vpc.id}"
+    cidr_block = var.subnet4-cidr
+    map_public_ip_on_launch = "true"
+    availability_zone = var.subent_az4
+    tags = {
+      Name = "Demo-web-public-subent-1b"
+    }
+  
+}
+
+resource "aws_subnet" "Demo-app-private-subent-1b" {
+    vpc_id = "${aws_vpc.Demo-vpc.id}"
+    cidr_block = var.subnet5-cidr
+    map_public_ip_on_launch = "true"
+    availability_zone = var.subent_az5
+    tags = {
+      Name = "Demo-app-private-subent-1b"
+    }
+  
+}
+
+resource "aws_subnet" "Demo-db-private-subent-06" {
+    vpc_id = "${aws_vpc.Demo-vpc.id}"
+    cidr_block = var.subnet6-cidr
+    map_public_ip_on_launch = "true"
+    availability_zone = var.subent_az6
+    tags = {
+      Name = "Demo-db-private-subent-1b"
+    }
+  
+}
+
+//Creating a Internet Gateway
+
+resource "aws_internet_gateway" "Demo-igw" {
+    vpc_id = "${aws_vpc.Demo-vpc.id}"
+    tags = {
+      Name = "Demo-igw"
+    }
+}
+
+// Create a route table 
+
+resource "aws_route_table" "Demo-public-rt" {
+    vpc_id = "${aws_vpc.Demo-vpc.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.Demo-igw.id}"
+    }
+    tags = {
+      Name = "Demo-public-rt"
+    }
+}
+
+// Associate subnet with routetable 
+
+resource "aws_route_table_association" "Demo-rta-public-subent-1" {
+    subnet_id = "${aws_subnet.Demo-web-public-subent-1a.id}"
+    route_table_id = "${aws_route_table.Demo-public-rt.id}"
+  
+}
+
+resource "aws_route_table_association" "Demo-rta-public-subent-2" {
+    subnet_id = "${aws_subnet.Demo-web-public-subent-1b.id}"
+    route_table_id = "${aws_route_table.Demo-public-rt.id}"
+  
+}
+// create a security group
+
+resource "aws_security_group" "Jenkins-sg" {
+  name        = "Jenkins-sg"
+  vpc_id      = aws_vpc.Demo-vpc.id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
     Name = "Jenkins-sg"
-  }
-}
-
-
-resource "aws_instance" "web" {
-  ami                    = "ami-0f5ee92e2d63afc18"
-  instance_type          = "t2.medium"
-  key_name               = "Mumbai"
-  vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
-  user_data              = templatefile("./install_jenkins.sh", {})
-
-  tags = {
-    Name = "Jenkins-sonar"
-  }
-  root_block_device {
-    volume_size = 8
   }
 }
