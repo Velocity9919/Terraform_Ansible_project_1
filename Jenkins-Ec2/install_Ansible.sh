f0cf59c6a868c2151b7bb82ca30ec8d7899b99de
Velocity9919 <143435067+Velocity9919@users.noreply.github.com>
2023-10-31 21:16:49 +0530

    Update and rename install_jenkins.sh to install_Ansible.sh

--- Jenkins-Ec2/install_Ansible.sh
+++ Jenkins-Ec2/install_Ansible.sh
@@ -8,6 +8,6 @@ sudo apt install ansible -y
 sudo apt install git -y 
 mkdir Ansible && cd Ansible
 pwd
-git clone https://github.com/Aj7Ay/ANSIBLE.git
+git https://github.com/Velocity9919/Terraform_Ansible_project_1.git
 cd ANSIBLE
 ansible-playbook -i localhost Jenkins.yml
